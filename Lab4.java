14a1861fc1f182ed81d781e27bd78841bd5aa4ff
James Martin <jamesmartin604@gmail.com>
2024-10-23 13:44:51 +0100

    Create Lab4.java

---
+++ Lab4.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Lab4
+{
+    public static void main(String[]args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter how many words: ");
+        int size = scanner.nextInt();
+
+        String[] array = new String[size];
+
+        for(int i = 0; i < size; i++)
+        {
+            System.out.println("Enter the word at pos: " + (i+1));
+            array[i] = scanner.next();
+        }
+
+        System.out.println("Unsorted array: ");
+        printArray(array);
+        sort(array);
+        System.out.println("Sorted array: ");
+        printArray(array);
+
+    }
+
+
+    public static void sort(String[] arr)
+    {
+        boolean swapped;
+
+        for(int i = 0; i < arr.length; i++)
+        {
+            swapped = false;
+            for(int j = 0; j < arr.length - i - 1; j++)
+            {
+                if(arr[j].length() > arr[j+1].length())
+                {
+                    String temp;
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped)
+            {
+                break;
+            }
+        }
+    }
+
+
+    public static void printArray(String[] arr)
+    {
+        for(int i = 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
