5833817e6efbfe94512cd75372c5a76c3a791681
James Martin <jamesmartin604@gmail.com>
2024-11-09 16:31:47 +0000

    Update Lab5.java

--- Lab5.java
+++ Lab5.java
@@ -1,23 +1,66 @@
-import java.util.Stack;
 import java.util.Scanner;
+import java.util.Stack;
 
-public class stacks
+public class cs210lab5
 {
-    public static void main(String[]args)
+    public static void main(String[] args) 
     {
         Scanner scanner = new Scanner(System.in);
-        //Create a new stack
         Stack<String> stack = new Stack<>();
+
+        // Define allowed inputs
+        String[] allowedInputs = {"Go North", "Go East", "Go West", "Go South", "Go Back", "Arrived"};
+        
+        // Main loop
+        while (true) 
+        {            
+            String input = scanner.nextLine();
+
+            // Break loop if user types "exit"
+            if (input.equalsIgnoreCase("Arrived")) 
+            {
+                break;
+            }
+
+
+
+           
+
+            // Check if the input is in the list of allowed inputs
+            boolean isAllowed = false;
+            for (String allowed : allowedInputs) 
+            {
+                if (input.equalsIgnoreCase(allowed)) 
+                {
+                    isAllowed = true;
+                    break;
+                }
+            }
+
+            // Add to stack if allowed, otherwise show error message
+            if (isAllowed) 
+            {
+                stack.push(input);    
+                if(input.equals("Go Back"))
+                {
+                    stack.pop();
+                    if(!stack.isEmpty())
+                    {
+                        stack.pop();
+                    }
+                }            
+            } 
+            else 
+            {
+                System.out.println("Invalid input.");
+            }
+        }
+        
+        System.out.println(stack);
+        scanner.close();
+    }
 
     
 
-        
-        while(!stack.contains("Arrived"))
-        {
-            stack.push(scanner.next());
-        }
-        System.out.println(stack);
-
-       
-    }
+   
 }
