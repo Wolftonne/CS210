4998c0ff2ea6e8d29d51d3e313d968c26b4085c8
James Martin <jamesmartin604@gmail.com>
2024-11-12 11:47:01 +0000

    Update priorityQueueTest.java

--- priorityQueueTest.java
+++ priorityQueueTest.java
@@ -1,17 +1,27 @@
-//import java.util.Queue;
+import java.util.Scanner;
 
 public class priorityQueueTest
 {
     public static void main(String[] args)
-    {
-        priorityQueue q1 = new priorityQueue(10); // make new p queue
-        q1.insert(60); // slot items into queue
-        q1.insert(20);
-        q1.insert(80);
-        q1.insert(40);
+    {       
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size of queue: ");
+        int size = scanner.nextInt();
+        priorityQueue q1 = new priorityQueue(size);
+
+        for(int i = 0; i < size; i++)
+        {
+            System.out.println("Enter value at pos " + (i+1));
+            q1.insert(scanner.nextLong());
+        }
+
+        q1.remove();
+        
+
+        System.out.println();
         while(!q1.isEmpty())
         {
             System.out.println(q1.remove());
-        }        
+        }
     }
 }
