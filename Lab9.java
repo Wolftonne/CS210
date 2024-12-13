8060699e164fa386ef17fa2e76aef1be1ef57ce7
James Martin <jamesmartin604@gmail.com>
2024-12-13 14:40:30 +0000

    Update Lab9.java

--- Lab9.java
+++ Lab9.java
@@ -11,21 +11,18 @@ public class lab9 {
         int R[] = new int[n2];
 
         //copy data to temp arrays
-        for(int i = 0; i < n1; i++) {
-            L[i] = workSpace[first+i];
+        for (int i = 0; i < n1; i++) {
+            L[i] = workSpace[first + i];
         }
-        for(int j = 0; j < n2; j++) {
+        for (int j = 0; j < n2; j++) {
             R[j] = workSpace[second + 1 + j];
         }
-        
-        //merge the temp arrays
-        //inital indices of first and second subarrays
-        int i = 0, j = 0;
 
-        //initial index of merged subarray array
+        //merge the temp arrays
+        int i = 0, j = 0;
         int k = first;
-        while(i < n1 && j < n2) {
-            if(L[i] <= R[j]) {
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
                 workSpace[k] = L[i];
                 i++;
             } else {
@@ -35,69 +32,68 @@ public class lab9 {
             k++;
         }
         //copy any remaining elements of L[] if any
-        while(i < n1) {
+        while (i < n1) {
             workSpace[k] = L[i];
             i++;
-            k++; 
+            k++;
         }
 
-        //copy remaining elems of R[] if any
-        while(j < n2) {
+        //copy remaining elements of R[] if any
+        while (j < n2) {
             workSpace[k] = R[j];
             j++;
             k++;
-        }        
+        }
     }
 
     //function that sorts workSpace[first...second] using merge()
     public static void sort(int workSpace[], int first, int second) {
-        if(first < second) {
+        if (first < second) {
             //find middle
             int mid = first + (second - first) / 2;
 
-            //sort first and second halves using insertion sort if first + second < 9
-            if((first + second) < 9) {
-                insertionSort(workSpace);
+            //sort first and second halves using insertion sort if subsection size < 9
+            if ((second - first + 1) < 9) {
+                insertionSort(workSpace, first, second);
             } else {
                 sort(workSpace, first, mid);
                 sort(workSpace, mid + 1, second);
+                merge(workSpace, first, mid, second);
             }
         }
     }
 
-    public static void insertionSort(int arr[]) {
-        int n = arr.length;
-        for(int i = 1; i < n; i++) {
+    public static void insertionSort(int arr[], int start, int end) {
+        for (int i = start + 1; i <= end; i++) {
             int key = arr[i];
             int j = i - 1;
-            while(j>=0&&arr[j]>key) {
-                arr[j+1] = arr[j];
+            while (j >= start && arr[j] > key) {
+                arr[j + 1] = arr[j];
                 j = j - 1;
             }
             arr[j + 1] = key;
         }
     }
 
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Random rand = new Random();        
-        int size = 500;        
+        Random rand = new Random();
+        int size = 500;
         int arr[] = new int[size]; //create array of size 500
 
         //populate array with 500 random numbers
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             arr[i] = rand.nextInt(200);
-        }        
-        sort(arr, 0, size-1);
+        }
+        sort(arr, 0, size - 1);
+
         System.out.print("[");
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             System.out.print(arr[i]);
-            if(i < size - 1) {
+            if (i < size - 1) {
                 System.out.print(", ");
             }
-        }  
+        }
         System.out.print("]");
-
     }
 }
